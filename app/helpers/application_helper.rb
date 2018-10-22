c9f1c6f6bbf803e8ad92ccea987ab4e40d671178
Dailton Bastos <dailtonbastos@gmail.com>
2018-10-21 23:00:08 -0300

    Criado um helper para data e nome do App

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Criando um helper para data e hora
+  def data_br(data_us)
+    data_us.strftime("%d/%m/%Y")
+  end
+
+  def nome_aplicacao
+    "Crypto Wallet"
+  end
+  
 end
