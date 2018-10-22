7c617582dc1a221b742d558f4d51114d59c126de
Dailton Bastos <dailtonbastos@gmail.com>
2018-10-22 11:34:24 -0300

    Ambiente Rails, Variável de sessão, Query params

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,5 @@
 class WelcomeController < ApplicationController
   def index
+    @nome_curso = params[:nome]#Variável de sessão
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,8 +4,19 @@ module ApplicationHelper
     data_us.strftime("%d/%m/%Y")
   end
 
+  # Helper que retorna o nome da App
   def nome_aplicacao
     "Crypto Wallet"
   end
   
+  #Helper que indentifica o ambiente Rails
+  def ambiente_rails
+    if Rails.env.development?
+      "Desenvolvimento"
+    elsif Rails.env.production?
+      "Produção"
+    else
+      "Teste"
+    end
+  end
 end
