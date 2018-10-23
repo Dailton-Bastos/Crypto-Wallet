104fa2ee1d2c273bf3fdb691e64bd16706f99ac5
Dailton Bastos <dailtonbastos@gmail.com>
2018-10-23 14:12:11 -0300

    Corrigindo o segundo CRUD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_23_024356) do
+ActiveRecord::Schema.define(version: 2018_10_23_170848) do
 
   create_table "coins", force: :cascade do |t|
     t.string "description"
@@ -21,7 +21,7 @@ ActiveRecord::Schema.define(version: 2018_10_23_024356) do
   end
 
   create_table "mining_types", force: :cascade do |t|
-    t.string "name"
+    t.string "description"
     t.string "acronym"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
