1ad17b473197fea431904e854641c4e55f5bf986
Dailton Bastos <dailtonbastos@gmail.com>
2018-10-26 15:30:46 -0300

    Adicionando multiplos idiomas na aplicação

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::Base
+  #Criando um métdo p/ usar multiplos idiomas na App
+  before_action :set_locale
+  
+  def set_locale
+    if params[:locale]
+      cookies[:locale] = params[:locale]
+    end
+    if cookies[:locale]
+      if I18n.locale != cookies[:locale]
+        I18n.locale = cookies[:locale]
+      end
+    end
+  end  
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    #cookies e sessions
+    cookies[:curso] = "Curso de Ruby on Rails [COOKIE]"
+    session[:curso] = "Curso de Ruby on Rails [SESSION]"
     @nome_curso = params[:nome]#Variável de sessão
+    @curso = params[:curso]
   end
 end
